fd3ef241ae30d73caff8b8e239700abdb161778a
Alex Blandin <20842735+AlexBlandin@users.noreply.github.com>
2020-12-24 10:47:47 +0000

    Ready for graphing

--- graph.py
+++ graph.py
@@ -1,12 +1,18 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from pathlib import Path
 
-for spec in ["jitter-confuse", "jitter", "confuse"]:
-  for f in Path("results/").glob(f"*-{spec}-0*.csv"):
-    df=pd.read_csv(f) # no index_col as epoch is time-series data, not id
-    
+specs = ["-jitter-confuse", "-jitter", "-confuse"]
+for spec in specs:
+  models = {}
+  for f in Path("results/").glob(f"*{spec}-0*.csv"):
+    if spec == specs[0] or (spec != specs[0] and (specs[1] not in f.name or specs[2] not in f.name)): # so we don't duplicate      
+      model = f.name.split(spec)[0]
+      df = pd.read_csv(f)
+      acc = df["val_categorical_accuracy"] # the only one we care about
+      if model not in models or (model in models and acc[9] > models[model][9]): # since we're only plotting 10 epochs worth
+        models[model] = acc[:10]
+  df = pd.DataFrame({"epochs":list(range(10)), **models})
+  df.to_csv(f"acc{spec}.csv",index=False)
 
 # Overall
 # - Validation Accuracy over Ten Epochs for All Models
